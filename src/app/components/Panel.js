a645d5e96a886fd8ccdbd3f7e8bf69556e59e5bb
Will Hart <hart.wl@gmail.com>
2020-08-23 16:38:03 +1000

    Format header and main sections

--- src/app/components/Panel.js
+++ src/app/components/Panel.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FiActivity } from "react-icons/fi";
 import JSONTree from 'react-json-tree'
 import { Sparklines, SparklinesLine, SparklinesSpots, SparklinesReferenceLine } from 'react-sparklines'
 
@@ -59,8 +60,8 @@ export class Panel extends Component {
     this.fpsUpdateCounter++
 
     if (this.fpsUpdateCounter > FPS_SMOOTHING) {
-      let fps = [...this.state.fps, this.fpsTotal / FPS_SMOOTHING]
-      fps = fps.slice(fps.length - FPS_SMOOTHING)
+      let fps = [...this.state.fps, Math.floor(this.fpsTotal / FPS_SMOOTHING)]
+      fps = fps.slice(Math.max(0, fps.length - FPS_SMOOTHING))
 
       this.fpsUpdateCounter = 0
       this.fpsTotal = 0
@@ -84,15 +85,15 @@ export class Panel extends Component {
       <div class="wrapper">
         <header>
           <div className="stat">
-            <span className="stat-value">{Math.floor(fps[fps.length - 1] || 0)}</span>
+            <span className="stat-value">{fps[fps.length - 1] || '?'}</span>
             <span className="stat-label">FPS</span>
           </div>
           <div className="thin-sparkline">
-            <Sparklines data={fps} limit={20} width="150" height="40">
+            {fps.length < 20 ? <FiActivity /> : <Sparklines data={fps} limit={20} width="150" height="40" min={0} max={150}>
               <SparklinesLine color="#1c8cdc" />
               <SparklinesSpots />
               <SparklinesReferenceLine type="mean" />
-            </Sparklines>
+            </Sparklines>}
           </div>
         </header>
 
